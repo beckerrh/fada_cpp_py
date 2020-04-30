a507cb7a7ad27a53c0dfef2fb0bffb607c2eb2ca
Roland Becker <roland.becker@univ-pau.fr>
2020-04-30 10:20:54 +0200

    Initial Commit

---
+++ Fada/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fada
+//
+//  Created by Roland Becker on 30/04/2020.
+//  Copyright © 2020 Roland Becker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
